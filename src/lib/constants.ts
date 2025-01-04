5e88157e57939528c9f0ac3c980da606b0c9a0cb
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-01-04 08:15:31 +0700

    dummy

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const useCases = [
+  {
+    case: "WORK",
+    title: "For work",
+  },
+  {
+    case: "STUDY",
+    title: "For study",
+  },
+  {
+    case: "PERSONAL_USE",
+    title: "For personal use",
+  },
+];
+
+export const steps = [1, 2, 3, 4];
